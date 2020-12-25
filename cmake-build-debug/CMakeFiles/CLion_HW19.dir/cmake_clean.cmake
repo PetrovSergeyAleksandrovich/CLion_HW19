48c46c8813f0878036881e96328b7125e05bf368
PetrovSergeyAleksandrovich <ps@dsys.ru>
2020-12-25 17:21:45 +0300

    init commit

---
+++ cmake-build-debug/CMakeFiles/CLion_HW19.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CLion_HW19.exe"
+  "CLion_HW19.exe.manifest"
+  "CLion_HW19.lib"
+  "CLion_HW19.pdb"
+  "CMakeFiles/CLion_HW19.dir/main.cpp.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CLion_HW19.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
