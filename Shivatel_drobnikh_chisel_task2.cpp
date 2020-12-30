0ca972e2ae0b103b4e38b7af2f3897f60fd09143
PetrovSergeyAleksandrovich <ps@dsys.ru>
2020-12-30 19:53:38 +0300

    Shivatel_drobnikh_chisel task 2 done

--- Shivatel_drobnikh_chisel_task2.cpp
+++ Shivatel_drobnikh_chisel_task2.cpp
@@ -4,6 +4,31 @@
 int main()
 {
     std::string var;
+    std::stringstream text_buf;
+    double output, test_ref_1=1.1, test_ref_2=0.001;
 
+    std::cout << "Type first part: ";
+    std::cin >> var;
+    text_buf << var << ".";
 
+    std::cout << "Type second part: ";
+    std::cin >> var;
+    text_buf << var;
+
+    text_buf >> output;
+
+    //check test values for 1 and 1 inputs
+    if(output == test_ref_1)
+    {
+        std::cout << "output now is DOUBLE type" << std::endl;
+    }
+
+    //real output
+    std::cout<< "You have DOUBLE: " << output << std::endl;
+
+    // second check
+    output -= test_ref_2;
+    std::cout << "output is DOUBLE with -" << test_ref_2 << " : " << output << std::endl;
+
+    return 1234;
 }
