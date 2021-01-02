ff709747e1f37e5f9bf77ef27f1861fce91cab1c
PetrovSergeyAleksandrovich <ps@dsys.ru>
2021-01-02 14:25:16 +0300

    Piano_task_4 done

--- piano_task_4.cpp
+++ piano_task_4.cpp
@@ -2,43 +2,35 @@
 #include <sstream>
 #include <string>
 
-enum notes {DO = 1, RE, MI, FA, SOL, LA, SI};
+enum notes {DO=49, RE, MI, FA, SOL, LA, SI};
 
 int main()
 {
-
     std::stringstream input_buf;
     std::string text;
-    char * buf[512]={};
     int x;
 
     std::cout << "Type 12 note combination by pressing numbers from 1 to 7" << std::endl;
 
-    for(int i=0; i < 3; i++)
+    for(int i=0; i < 12; i++)
     {
         std::cout << "Combination " << i << " :";
         std::cin >> x;
         input_buf << x;
     }
 
-    std::cout << input_buf.str() << std::endl;
+    input_buf >> text;
 
-
-
-
-
-/*
-    notes single_note = DO;
-    if(single_note == 1)
+    for(int i=0; i < text.length(); i++)
     {
-        std::cout << "DO" << " ";
+        if(text[i] == DO) std::cout << "DO" << " ";
+        if(text[i] == RE) std::cout << "RE" << " ";
+        if(text[i] == MI) std::cout << "MI" << " ";
+        if(text[i] == FA) std::cout << "FA" << " ";
+        if(text[i] == SOL)std::cout << "SOL"<< " ";
+        if(text[i] == LA) std::cout << "LA" << " ";
+        if(text[i] == SI) std::cout << "SI" << " ";
     }
 
-    single_note = RE;
-    if(single_note == 2)
-    {
-        std::cout << "RE" << " ";
-    }
-*/
     return 0;
 }
